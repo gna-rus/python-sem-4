336169bf2cf42133d32d5ce7d2ae775893926470
Nikolay Grigoritev <gnarus@inbox.ru>
2023-04-19 14:49:59 +0300

    3 commit

--- Task24.py
+++ Task24.py
@@ -31,6 +31,7 @@ try:
     print(list1)
     save_position = 0
     sum_position = 0
+
     # Цикл перебора всех кустов и начинается он с 1 индекса и заканчивается цикл предпоследним индексом
     for i in range(1, len(list1) - 1):
         if (list1[i - 1] + list1[i] + list1[i + 1]) > sum_position:
